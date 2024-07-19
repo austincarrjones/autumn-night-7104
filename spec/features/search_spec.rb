db1c3b32a1da61d58b51e426aba13838537da936
Austin Carr-Jones <austincarr.jones@gmail.com>
2024-07-19 09:19:23 -0600

    pass first test

--- spec/features/search_spec.rb
+++ spec/features/search_spec.rb
@@ -4,12 +4,18 @@ RSpec.describe "Search Index" do
   context "as a user when I visit '/' and select 'Fire Nation' from the select field and click 'Search for Memebers'" do
     describe "basic routing" do
       it "should route to '/search' and I should see the total number of people who live in the Fire Nation" do
+        visit root_path
 
+        select "Fire Nation", from: "nation"
+        click_button "Search For Members"
+
+        expect(current_path).to eq(search_path)
       end
 
       xit "should display a list with detailed information for the first 25 members of the Fire Nation" do
 
       end
+    end
 
     describe "detailed list for each member" do
       xit "should show the name of the member (and their photo, if they have one)" do
